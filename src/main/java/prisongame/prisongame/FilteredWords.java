0c0640d89a198c2d44ba9f3972b50884e6c38892
honkling <untitledgeese@gmail.com>
2023-01-15 14:31:41 -0600

    Filter improvements

--- src/main/java/prisongame/prisongame/FilteredWords.java
+++ src/main/java/prisongame/prisongame/FilteredWords.java
@@ -1,21 +1,50 @@
 package prisongame.prisongame;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class FilteredWords {
     static String[] filter = {
-            "nigger",
+            "niga",
             "niger",
-            "nigga",
-            "faggot",
-            "fagot",
-            "retard"
+            "niba",
+            "niber",
+            "noga",
+            "noger",
+            "retard",
+            "kys",
+            "fag",
+            "vagina",
+            "pusy",
+            "dick",
+            "cock",
+            "penis",
+            "sex"
     };
 
+    private static String replaceConsecutiveDuplicates(String msg) {
+        final Pattern pattern = Pattern.compile("(.)\\1*", Pattern.MULTILINE);
+        final Matcher matcher = pattern.matcher(msg);
+
+        StringBuilder sanitized = new StringBuilder();
+
+        while (matcher.find())
+            sanitized.append(matcher.group(1));
+
+        return sanitized.toString();
+    }
+
     public static String filtermsg(String msg) {
+        String sanitized = replaceConsecutiveDuplicates(msg
+                .replaceAll("\\s+", ""));
+
+        System.out.printf("Sanitized (%s)\n", sanitized);
+
         for (String i : filter) {
-            if (msg.toLowerCase().contains(i)) {
-                msg = "I FUCKING LOVE AMONG US!!! YESS!!! AMONGER!! SUSS!!! SUSSY!!! SUSSY BAKA!! SUSS!! WALTUH!! KINDA SUS WALTUH!!";
-            }
+            if (sanitized.toLowerCase().contains(i))
+                return "I FUCKING LOVE AMONG US!!! YESS!!! AMONGER!! SUSS!!! SUSSY!!! SUSSY BAKA!! SUSS!! WALTUH!! KINDA SUS WALTUH!!";
         }
+
         return msg;
     }
 
