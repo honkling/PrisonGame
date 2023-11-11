5b010724a88b1483e681e7316782145ecdc4763b
honkling <untitledgeese@gmail.com>
2023-11-11 15:17:33 -0600

    fix: passing to a friend when in peril

--- src/main/java/prisongame/prisongame/commands/warden/PassCommand.java
+++ src/main/java/prisongame/prisongame/commands/warden/PassCommand.java
@@ -13,6 +13,16 @@ import prisongame.prisongame.lib.Role;
 public class PassCommand implements CommandExecutor {
     @Override
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
+        if (!(sender instanceof Player player)) {
+            sender.sendMessage(PrisonGame.mm.deserialize("<red>Only players can execute this command."));
+            return true;
+        }
+
+        if (player.getHealth() < 10) {
+            sender.sendMessage(PrisonGame.mm.deserialize("<red>You cannot pass under 5 hearts."));
+            return true;
+        }
+
         if (args.length >= 1) {
             if (args[0].equalsIgnoreCase(sender.getName())) {
                 sender.sendMessage(ChatColor.RED + "You cannot pass to yourself.");
