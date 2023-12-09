4355069162c47fbb40bf2b23bdd9664975ee7a70
honkling <untitledgeese@gmail.com>
2023-12-09 12:43:11 -0600

    fix: /gangs help

--- src/main/java/prisongame/prisongame/commands/gangs/HelpCommand.java
+++ src/main/java/prisongame/prisongame/commands/gangs/HelpCommand.java
@@ -18,18 +18,18 @@ public class HelpCommand implements IGangCommand {
                 /gangs create (name) <dark_gray>- <gray>Create a gang.</dark_gray>
                 /gangs bank <dark_gray>- <gray>View bank information.</dark_gray>
                 /gangs bank deposit (amount) <dark_gray>- <gray>Deposit cash into the bank.</dark_gray>
-                /gangs bank withdraw (amount) <dark_gray>- <gray>Withdraw cash from the bank.</gray>
-                /gangs leaderboard <dark_gray>- <gray>View a leaderboard of gangs.</gray>
-                /gangs info [gang] <dark_gray>- <gray>View information about a gang.</gray>
-                /gangs invite (player) <dark_gray>- <gray>Invite a player to the gang.</gray>
-                /gangs official (player) <dark_gray>- <gray>Promote a player to an official.</gray>
-                /gangs fire (player) <dark_gray>- <gray>Fire a player.</gray>
-                /gangs kick (player) <dark_gray>- <gray>Kick a player from the gang.</gray>
-                /gangs resign <dark_gray>- <gray>Resign as an official.</gray>
-                /gangs accept <dark_gray>- <gray>Accept an invitation to be a member or official.</gray>
-                /gangs leave <dark_gray>- <gray>Leave the gang.</gray>
-                /gangs join (gang) <dark_gray>- <gray>Request to join a gang.</gray>
-                /gangs disband <dark_gray>- <gray>Disband the gang.</gray>
+                /gangs bank withdraw (amount) <dark_gray>- <gray>Withdraw cash from the bank.</dark_gray>
+                /gangs leaderboard <dark_gray>- <gray>View a leaderboard of gangs.</dark_gray>
+                /gangs info [gang] <dark_gray>- <gray>View information about a gang.</dark_gray>
+                /gangs invite (player) <dark_gray>- <gray>Invite a player to the gang.</dark_gray>
+                /gangs official (player) <dark_gray>- <gray>Promote a player to an official.</dark_gray>
+                /gangs fire (player) <dark_gray>- <gray>Fire a player.</dark_gray>
+                /gangs kick (player) <dark_gray>- <gray>Kick a player from the gang.</dark_gray>
+                /gangs resign <dark_gray>- <gray>Resign as an official.</dark_gray>
+                /gangs accept <dark_gray>- <gray>Accept an invitation to be a member or official.</dark_gray>
+                /gangs leave <dark_gray>- <gray>Leave the gang.</dark_gray>
+                /gangs join (gang) <dark_gray>- <gray>Request to join a gang.</dark_gray>
+                /gangs disband <dark_gray>- <gray>Disband the gang.</dark_gray>
                 </blue>-=-=-=-=-=-=-=-</dark_blue>
                 """));
         return true;
