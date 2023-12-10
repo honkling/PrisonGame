a4c86b7a608ade410ec68a1b61d2f6b4e6126a4e
honkling <untitledgeese@gmail.com>
2023-12-10 09:01:57 -0600

    fix: check y for bounds

--- src/main/java/prisongame/prisongame/PrisonGame.java
+++ src/main/java/prisongame/prisongame/PrisonGame.java
@@ -664,12 +664,20 @@ public final class PrisonGame extends JavaPlugin {
 
     public static boolean isInside(Player player, Location loc1, Location loc2)
     {
+
+
         double[] dim = new double[2];
 
         dim[0] = loc1.getX();
         dim[1] = loc2.getX();
         Arrays.sort(dim);
         if(player.getLocation().getX() > dim[1] || player.getLocation().getX() < dim[0])
+            return false;
+
+        dim[0] = loc1.getY();
+        dim[1] = loc2.getY();
+        Arrays.sort(dim);
+        if(player.getLocation().getY() > dim[1] || player.getLocation().getY() < dim[0])
             return false;
 
         dim[0] = loc1.getZ();
