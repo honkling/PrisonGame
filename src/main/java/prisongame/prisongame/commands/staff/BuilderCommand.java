16f67e195eac70c9c1786bc196c4d56018b1cb88
honkling <untitledgeese@gmail.com>
2023-12-18 13:12:50 -0600

    feat: builder if creative

--- src/main/java/prisongame/prisongame/commands/staff/BuilderCommand.java
+++ src/main/java/prisongame/prisongame/commands/staff/BuilderCommand.java
@@ -4,6 +4,7 @@ import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.minimessage.tag.resolver.Placeholder;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.GameMode;
 import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
@@ -17,7 +18,7 @@ public class BuilderCommand implements CommandExecutor {
     // This method is called, when somebody uses our command
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        if (!(sender instanceof Player player) || !player.hasPermission("minecraft.command.gamemode")) {
+        if (!(sender instanceof Player player) || !player.hasPermission("minecraft.command.gamemode") || player.getGameMode() != GameMode.CREATIVE) {
             sender.sendMessage("You aren't a builder!");
             return true;
         }
